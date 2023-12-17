6b5372a24d48fe4a87ba13e4359d3d780bb13b36
Owner <noaevron2017@gmail.com>
2023-12-17 15:53:46 +0200

    Added comment

--- app/src/main/java/com/example/myapplicationn/AppConstants.java
+++ app/src/main/java/com/example/myapplicationn/AppConstants.java
@@ -3,12 +3,15 @@ package com.example.myapplicationn;
 public class AppConstants
 {
 
-    public static final int RADIUS = 500;
+    public static final int RADIUS = 450;
     public static final float X = 900;
-    public static final float Y = 700;
+    public static final float Y1 = 600;
+    public static final float Y2 = 1650;
 
     public static final float[] arrX = {X, (float) (X - 0.7*RADIUS), (float) ((X - RADIUS) + (0.1 * RADIUS)), (float) (X - 0.5*RADIUS), X, (float) (X + 0.5 * RADIUS), (float) ((X + RADIUS) - (0.3 * RADIUS)), (float) (X + 0.5*RADIUS)};
-    public static final float[] arrY = {(float) ((Y - RADIUS) + (0.1*RADIUS)), (float) (Y-0.6*RADIUS), Y, (float) (Y+ 0.6*RADIUS), (float) ((Y + RADIUS) - (0.3*RADIUS)), (float) (Y+ 0.5*RADIUS), Y, (float) (Y - 0.7*RADIUS)};
+    public static final float[] arrY1 = {(float) ((Y1 - RADIUS) + (0.1*RADIUS)), (float) (Y1-0.6*RADIUS), Y1, (float) (Y1+ 0.6*RADIUS), (float) ((Y1 + RADIUS) - (0.3*RADIUS)), (float) (Y1+ 0.5*RADIUS), Y1, (float) (Y1 - 0.7*RADIUS)};
+    public static final float[] arrY2 = {(float) ((Y2 - RADIUS) + (0.1*RADIUS)), (float) (Y2-0.6*RADIUS), Y2, (float) (Y2+ 0.6*RADIUS), (float) ((Y2 + RADIUS) - (0.3*RADIUS)), (float) (Y2+ 0.5*RADIUS), Y2, (float) (Y2 - 0.7*RADIUS)};
+
 
 
 }
--- app/src/main/java/com/example/myapplicationn/Board.java
+++ app/src/main/java/com/example/myapplicationn/Board.java
@@ -20,8 +20,9 @@ public class Board extends View
     float x, y, r;
     Paint paint;
     Bitmap bitmap;
+    int curretCard = 0;
 
-    MyCircle circle;
+    MyCircle circle1, circle2;
     Picture pic;
     int[] bitmaps;
 
@@ -31,28 +32,15 @@ public class Board extends View
     public Board(Context context)
     {
         super(context);
-        circle = new MyCircle(AppConstants.X, AppConstants.Y ,AppConstants.RADIUS);
+        circle1 = new MyCircle(AppConstants.X, AppConstants.Y1 ,AppConstants.RADIUS);
+        circle2 = new MyCircle(AppConstants.X, AppConstants.Y2 ,AppConstants.RADIUS);
         this.context = context;
-
-        //pic = new Picture();
-
-
         bitmaps = new int[]{R.drawable.airplane, R.drawable.artistpalette, R.drawable.ballet, R.drawable.bathtub, R.drawable.bomb, R.drawable.bowling, R.drawable.bubbles, R.drawable.camera, R.drawable.cheese, R.drawable.coral, R.drawable.corn, R.drawable.cyclone, R.drawable.doughnut, R.drawable.dragonface, R.drawable.firecracker, R.drawable.gloves, R.drawable.hat, R.drawable.infinity, R.drawable.jackolantern, R.drawable.joker, R.drawable.key, R.drawable.kite, R.drawable.locked, R.drawable.luggage, R.drawable.medal, R.drawable.microphone, R.drawable.money, R.drawable.moon, R.drawable.mountain, R.drawable.mushroom, R.drawable.nazaramulet, R.drawable.pistol, R.drawable.rocket, R.drawable.sandal, R.drawable.saxophone, R.drawable.shopping, R.drawable.snowflake, R.drawable.snowman, R.drawable.spider, R.drawable.sunglasses, R.drawable.tent, R.drawable.tiger, R.drawable.truck, R.drawable.tulip, R.drawable.umbrella, R.drawable.volcano, R.drawable.waterwave, R.drawable.wedding, R.drawable.yoyo};
-
-      //  deck.add(new Card());
-
         CreateDeck();
 
-        showCard(0);
         Toast.makeText(context,"" + CheckDeck(),Toast.LENGTH_LONG).show();
-
     }
 
-    private void showCard(int index) {
-        int[] arr = deck.get(index).getCard();
-
-
-    }
 
     public void CreateDeck()
     {
@@ -139,21 +127,23 @@ public class Board extends View
         {
         super.onDraw(canvas);
 
-        canvas.drawCircle(circle.getX(), circle.getY(), circle.getR(), circle.getPaint());
+        canvas.drawCircle(circle1.getX(), circle1.getY(), circle1.getR(), circle1.getPaint());
+        canvas.drawCircle(circle2.getX(), circle2.getY(), circle2.getR(), circle2.getPaint());
 // added comment
 
 
-            int[] arr = deck.get(0).getCard();
-        // draw card
-            for (int i = 0; i <arr.length ; i++)
+            int[] arr1 = deck.get(curretCard).getCard();
+            for (int i = 0; i <arr1.length ; i++) {
+                bitmap = BitmapFactory.decodeResource(getResources(), bitmaps[arr1[i]]);
+                canvas.drawBitmap(bitmap, AppConstants.arrX[i], AppConstants.arrY1[i], null);
+            }
+            if (curretCard == deck.size() - 1)
+                curretCard = 0;
+            int[] arr2 = deck.get(curretCard + 1).getCard();
+            for (int i = 0; i <arr2.length ; i++)
             {
-                bitmap = BitmapFactory.decodeResource(getResources(), bitmaps[arr[i]]);
-              //  bitmap.setWidth((int) AppConstants.arrX[i]);
-               // bitmap.setHeight((int) AppConstants.arrY[i]);
-
-                canvas.drawBitmap(bitmap, AppConstants.arrX[i] ,AppConstants.arrY[i], null);
-
-
+                bitmap = BitmapFactory.decodeResource(getResources(), bitmaps[arr2[i]]);
+                canvas.drawBitmap(bitmap, AppConstants.arrX[i], AppConstants.arrY2[i], null);
             }
     }
 
@@ -165,9 +155,42 @@ public class Board extends View
             float x = event.getX();
             float y = event.getY();
             Log.d("TOUCH", "onTouchEvent: " + x + "," + y);
+            // check which image
+            int i = findClickedImage(x,y);
+            Log.d("ON TOUCH ", "onTouchEvent: " + i );
+            if (i != -1)
+            {
+                boolean flag = CheckIfTrue(i);
+                Log.d("check ", "flag: " +  flag);
+            }
         }
 
         return super.onTouchEvent(event);
 
+    }
+
+    private boolean CheckIfTrue(int indx)
+    {
+        int[] arr1 = deck.get(curretCard).getCard();
+        int[] arr2 = deck.get(curretCard + 1).getCard();
+        for (int i = 0; i <arr1 .length ; i++)
+        {
+            if (arr1[indx] == arr2[i])
+                return true;
+        }
+        return  false;
+    }
+
+    private int findClickedImage(float x, float y)
+    {
+        int[] arr = deck.get(curretCard).getCard();
+        for (int i = 0; i <arr.length ; i++)
+        {
+            bitmap = BitmapFactory.decodeResource(getResources(), bitmaps[arr[i]]);
+
+            if ((x > AppConstants.arrX[i] && x < (AppConstants.arrX[i] + bitmap.getWidth())) &&(y > AppConstants.arrY1[i] && y < (AppConstants.arrY1[i] + bitmap.getHeight())))
+                return i;
+        }
+        return  -1;
     }
 }
