a806e10cc2dbd022dc5d11699629349689b3ffce
Owner <noaevron2017@gmail.com>
2024-01-24 10:51:36 +0200

    Added comment

--- app/src/main/java/com/example/myapplicationn/MainActivity.java
+++ app/src/main/java/com/example/myapplicationn/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplicationn;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,7 +18,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     LinearLayout linearLayout;
     Board board;
@@ -54,6 +57,15 @@ public class MainActivity extends AppCompatActivity {
                 tv.setVisibility(View.VISIBLE);
                 imageView.setVisibility(View.VISIBLE);
 
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // image view has been clicked
+                        setClickToChat("invited to join my game... Code for the game is:\n" + documentReference.getId());
+
+                    }
+                });
+
                 // 1 textview - visibilty - hidden
                 // 2 share icon
                 // 3 onclick shareicon
@@ -76,10 +88,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public  void setClickToChat(String message){
+        String url = "https://api.whatsapp.com/send";
+        try {
+            PackageManager pm = this.getPackageManager();
+            pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            i.putExtra(Intent.EXTRA_TEXT, message);
+
+            startActivity(i);
+        } catch (PackageManager.NameNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        }
+    }
+
+
     private void showGame() {
         linearLayout = (LinearLayout)findViewById(R.id.game);
 
         linearLayout.addView(board);
+
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
