b7163e234e15f118e0da9465cd25abec2ae03d9f
Amirdaniel22 <amirda2222@gmail.com>
2024-06-24 10:20:12 +0300

    הפרויקט לא גמור עדיין

---
+++ app/src/main/java/com/example/myapplicationn/User.java
@@ -0,0 +1,18 @@
+package com.example.myapplicationn;
+
+public class User
+{
+    int wins;
+    int losts;
+    int numGames;
+    String name;
+    //email
+
+    public User()
+    {
+        this.wins =0;
+        this.losts = 0;
+        this.numGames = 0;
+        this.name = "";
+    }
+}
